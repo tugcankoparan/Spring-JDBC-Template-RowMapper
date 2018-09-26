8c20837a1de23ee3e49e896d2a067de47012982b
Tuğcan Koparan <tugcankoparan10@gmail.com>
2018-09-26 15:04:27 +0300

    Update EmployeeDao.java

--- JdbcTemplateRowMapper/src/com/tugcankoparan/EmployeeDao.java
+++ JdbcTemplateRowMapper/src/com/tugcankoparan/EmployeeDao.java
@@ -16,16 +16,18 @@ public void setTemplate(JdbcTemplate template) {
 }
 
 public List<Employee> getAllEmployeesRowMapper(){
-	return template.query("select * from employee",new RowMapper<Employee>(){
+	return template.query("select * from employee",new RowMapper<List<Employee>>(){
+		List<Employee> empList = new ArrayList<Employee>();
 		@Override
 		public Employee mapRow(ResultSet rs, int rownumber) throws SQLException {
 			Employee e=new Employee();
 			e.setId(rs.getInt(1));
 			e.setName(rs.getString(2));
 			e.setSalary(rs.getInt(3));
-			return e;
+			empList.add(e);
 		}
 	});
+	return empList;
 }
 
 }
